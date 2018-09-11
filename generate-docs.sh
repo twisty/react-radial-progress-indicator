6d9c74d678ccf6d73fd18701e4b418bffdffb3c2
Tim Brayshaw <tim@brayshaw.com>
2018-09-11 22:28:06 +0100

    Use a different api doc generator.

---
+++ generate-docs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const reactDocs = require('react-docgen');
+const docsToMarkdown = require('react-docs-markdown');
+
+const docList = [
+  {
+    name: 'RadialProgressIndicator',
+    file: './src/animated.js',
+    out: './docs/api.md',
+  },
+];
+
+docList.map(item => {
+  var src = fs.readFile(item.file, 'utf8', (err, data) => {
+    if (err) throw err;
+    let api = reactDocs.parse(data);
+    let md = docsToMarkdown(api, item.name);
+    fs.writeFile(item.out, md, err => {
+      if (err) throw err;
+    });
+  });
+});
